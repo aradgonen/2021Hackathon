730ad43a9b43cf0392f7509dadf180f83e27c7bb
שי וינטה <shai.viente@gmail.com>
2021-08-03 19:53:08 +0300

    win exma page

--- frontend/src/components/Exam.js
+++ frontend/src/components/Exam.js
@@ -3,46 +3,103 @@ import * as Survey from "survey-react";
 import 'survey-react/survey.css';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
-function Exam() {
-    const [isCompleted, setIsComplete] = React.useState(true);
-    let json = {
-        questions: [
+function Exam(props) {
+    const [isCompleted, setIsComplete] = React.useState(false);
+
+    function completeHandler(data){
+      console.log(data)
+      setIsComplete(true)
+    }
+
+    let Jsonprops = {
+      title : "American History!!!",
+      description: "You are about to start Exam by history. <br/>Please click on <b>'Start Exam'</b> button when you are ready.",
+      questions : [
           {
-            type: "checkbox",
-            name: "car",
-            title: "What car are you driving?",
-            isRequired: true,
-            hasSelectAll: true,
-            hasNone: true,
-            noneText: "None of the above",
-            colCount: 4,
-            choicesOrder: "asc",
+              title: "When was the Civil War?",
+              choices: [
+                  "1750-1800", "1800-1850", "1850-1900", "1900-1950", "after 1950"
+              ],
+              correctAnswer: "1850-1900"
+          },
+          {
+              title: "Who said 'Give me liberty or give me death?'",
+              choices: [
+                  "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
+              ],
+              correctAnswer: "Patrick Henry"
+          },
+          {
+            title: "Who said 'Give me liberty or give me death?'",
             choices: [
-              "Ford",
-              "Tesla",
-              "Vauxhall",
-              "Volkswagen",
-              "Nissan",
-              "Audi",
-              "Mercedes-Benz",
-              "BMW",
-              "Peugeot",
-              "Toyota",
-              "Citroen"
-            ]
+                "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
+            ],
+            correctAnswer: "Patrick Henry"
+        },
+        {
+          title: "Who said 'Give me liberty or give me death?'",
+          choices: [
+              "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
+          ],
+          correctAnswer: "Patrick Henry"
+      }
+      ]
+  }
+
+    let completejson = {
+      title: Jsonprops.title,
+      showProgressBar: "bottom",
+      firstPageIsStarted: true,
+      startSurveyText: "Start Exam",
+      logo: "https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg",
+      logoPosition: "left",
+      pages: [
+          {
+              questions: [
+                  {
+                      type: "html",
+                      html: Jsonprops.description
+                  }
+              ]
           }
+      ],
+      completedHtml: "<h4>You have answered correctly <b>{correctedAnswers}</b> questions from <b>{questionCount}</b>.</h4>"
+  };
+
+
+  for (const questionnum in Jsonprops.questions){
+    let question = Jsonprops.questions[questionnum]
+    completejson.pages.push(
+      {
+        questions: [
+            {
+                type: "radiogroup",
+                title: question.title,
+                choicesMin: 1,
+                choicesMax: 2,
+                //isRequired: true,
+                choices: question.choices,
+                //description: "Survey Description",
+                correctAnswer: question.correctAnswer
+            }
         ]
-      };
-      const surveyRender = isCompleted ? (
+    }
+    )
+  }
+  completejson.completedHtml = "<h4>You have answered correctly <b>{correctedAnswers}</b> questions from <b>{questionCount}</b>.</h4>"
+
+  const onCompleteComponent = isCompleted ? (
+    <div>The component after onComplete event</div>
+  ) : null;
+
+      const surveyRender = (
         <Survey.Survey
-          json={json}
-          showCompletedPage={false}
-          onComplete={setIsComplete}
+          json={completejson}
+          showCompletedPage={true}
+          onComplete={completeHandler}
         />
-      ) : null;
-      const onCompleteComponent = isCompleted ? (
-        <div>The component after onComplete event</div>
-      ) : null;
+      )
+     
       return (
         <div>
           {surveyRender}
---
+++ frontend/src/components/jsonformat.js
@@ -0,0 +1,34 @@
+let Jsonprops = {
+    title : "American History!!!",
+    description: "You are about to start Exam by history. <br/>Please click on <b>'Start Exam'</b> button when you are ready.",
+    questions : [
+        {
+            title: "When was the Civil War?",
+            choices: [
+                "1750-1800", "1800-1850", "1850-1900", "1900-1950", "after 1950"
+            ],
+            correctAnswer: "1850-1900"
+        },
+        {
+            title: "Who said 'Give me liberty or give me death?'",
+            choices: [
+                "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
+            ],
+            correctAnswer: "Patrick Henry"
+        },
+        {
+          title: "Who said 'Give me liberty or give me death?'",
+          choices: [
+              "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
+          ],
+          correctAnswer: "Patrick Henry"
+      },
+      {
+        title: "Who said 'Give me liberty or give me death?'",
+        choices: [
+            "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
+        ],
+        correctAnswer: "Patrick Henry"
+    }
+    ]
+}
